0740d8de8b8fcf8249899a862a14439531dd427b
TheFreshestPumpkin <102326889+TheFreshestPumpkin@users.noreply.github.com>
2022-12-23 04:33:36 +0300

    Add files via upload

---
+++ scrypt/scrypt_2.js
@@ -0,0 +1,16 @@
+function changeImage(src,id)
+{
+    document.getElementById(id).src=src;
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("site_navigation").style.padding = "10px 0px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("site_navigation").style.padding = "25px 0px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
